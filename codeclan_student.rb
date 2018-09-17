352be482fc9d0ac17e0e731104fb6168db6d78ee
Graham-Stein <graham@guavoaviation.co.uk>
2018-09-17 15:25:44 +0100

    Completes Part A

--- codeclan_student.rb
+++ codeclan_student.rb
@@ -1,12 +1,21 @@
 class CodeClanStudent
 
-  attr_reader :student_name, :cohort
+  attr_reader :student_name, :cohort, :fav_language
+  attr_writer :student_name, :cohort, :fav_language
 
-  def initialize(student_name, cohort)
+  def initialize(student_name, cohort, fav_language=nil)
     @student_name = student_name
     @cohort = cohort
+    @fav_language = fav_language
   end
 
+  def response
+    return "#{@student_name} says, hello chaps!"
+  end
+
+  def favourite_language
+    return "I love #{@fav_language}"
+  end
 
 
 
--- specs/codeclan_student_spec.rb
+++ specs/codeclan_student_spec.rb
@@ -17,5 +17,31 @@ class TestCodeClanStudent < MiniTest::Test
     assert_equal("E29", student.cohort)
   end
 
+  def test_rename_student
+    student = CodeClanStudent.new("Big Bob", "E29")
+    student.student_name = "Steve"
+    assert_equal("Steve", student.student_name)
+  end
+
+
+  def test_rename_cohort
+    student = CodeClanStudent.new("Big Bob", "E3")
+    student.cohort = "E8"
+    assert_equal("E8", student.cohort)
+  end
+
+
+  def test_student_response
+    student = CodeClanStudent.new("Big Boobied Bertha", "E69")
+    assert_equal("Big Boobied Bertha says, hello chaps!", student.response)
+
+  end
+
+  def test_favourite_language
+    student = CodeClanStudent.new("Big Boobied Bertha", "E69", "Ruby")
+    assert_equal("I love Ruby", student.favourite_language)
+  end
+
+
 
 end
